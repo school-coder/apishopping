388fdfaa9b5c2d25a2c86ceb4617e0d822f0816a
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2018-10-05 16:37:50 +0530

    adding routing
    routing to gallery and support

--- apishopping/src/app/app-routing.module.ts
+++ apishopping/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { SupportsComponent } from './supports/supports.component';
 
 
 //This is my case 
 const routes: Routes = [
-   
+    {
+        path: 'gallery',
+        component: GalleryComponent
+      },
+      {
+        path: 'support',
+        component: SupportsComponent
+      }
 ];
 
 @NgModule({
