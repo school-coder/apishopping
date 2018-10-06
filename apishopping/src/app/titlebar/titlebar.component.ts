76a10ddd1d99617701fd902bc63e7b797a660704
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2018-10-06 19:19:35 +0530

    styling for top level navigation

--- apishopping/src/app/titlebar/titlebar.component.ts
+++ apishopping/src/app/titlebar/titlebar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-titlebar',
   templateUrl: './titlebar.component.html',
   styleUrls: ['./titlebar.component.css']
 })
+
 export class TitlebarComponent implements OnInit {
 
-  constructor() { }
+  currentUrl: string;
+
+  constructor(private router: Router) {
+      this.router.events.subscribe((_: NavigationEnd) => { 
+        if(!!_.url) {
+          this.currentUrl = _.url;
+          console.log("The current url is "+ this.currentUrl);
+        }
+      });
+      
+   }
 
   ngOnInit() {
+    
   }
 
 }
