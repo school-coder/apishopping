9d9f304eb696fc0d6a8d6406f123c3cde1a27988
Nagarajan Sivaraman (NAS) <Nagarajan.Sivaraman@softwareag.com>
2018-10-09 17:13:37 +0530

    check out page changes

--- apishopping/src/app/checkout/checkout.component.ts
+++ apishopping/src/app/checkout/checkout.component.ts
@@ -9,9 +9,15 @@ export class CheckoutComponent implements OnInit {
 
   title = "Check Out";
 
+  active_tab = "v-pills-apis-tab";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setActiveTab(active_tab: string){
+    this.active_tab = active_tab;
   }
 
 }
